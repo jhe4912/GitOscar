6e4f99fb22f84a343cd24a593831615ca967b0a4
jhe4912 <jhe2@csus.edu>
2021-04-03 15:48:58 -0700

    Added SQLHandler

--- src/csc130s06/teamdeuxtwoto/gitoscar/Main.java
+++ src/csc130s06/teamdeuxtwoto/gitoscar/Main.java
@@ -1,5 +1,7 @@
 package csc130s06.teamdeuxtwoto.gitoscar;
 
+
+import csc130s06.teamdeuxtwoto.gitoscar.database.SQLHandler;
 import csc130s06.teamdeuxtwoto.gitoscar.database.SQLite;
 
 public class Main
@@ -7,14 +9,21 @@ public class Main
 	// Change the file path to wherever you have the database file
 	private static String sqLiteFilePath = "Insert your oscarworthymovies.db filepath here";
 	
+	// Database related things
 	private static SQLite sqlite;
 	public static SQLite getSQL() { return sqlite; }
+	
+	private static SQLHandler sqlHandler = new SQLHandler();
+	public static SQLHandler getSQLHandler() { return sqlHandler; }
+	
+	// Etc Things
+	
 	
 	public static void main(String[] arg)
 	{
 		System.out.println("Program Started");
 		
-		sqlite = new SQLite(sqLiteFilePath);
+		sqlite = new SQLite(SQLITEFILEPATH);
 		sqlite.close();
 		
 		System.out.println("Program Ended");
--- src/csc130s06/teamdeuxtwoto/gitoscar/database/SQLHandler.java
+++ src/csc130s06/teamdeuxtwoto/gitoscar/database/SQLHandler.java
@@ -1,5 +1,12 @@
 package csc130s06.teamdeuxtwoto.gitoscar.database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import csc130s06.teamdeuxtwoto.gitoscar.Main;
+
 public class SQLHandler
 {
 	/*
@@ -16,4 +23,27 @@ public class SQLHandler
 	 */
 	
 	
+	// This method is used only for testing the database.
+	public List<String> getAllAwardsFromCeremonyNumber(int ceremony) throws SQLException
+	{
+		SQLite sql = Main.getSQL();
+		sql.refreshConnection();
+		
+		List<String> awardsAndInfo = new ArrayList<String>();
+		ResultSet rs = sql.query("SELECT * FROM oscars WHERE ceremony='" + ceremony + "'");
+		
+		while (rs.next())
+		{
+			awardsAndInfo.add(
+					"Film year: " + rs.getString("yearFilm") + " - " +
+					"Ceremony year: " + rs.getString("yearCeremony") + " - " +
+					"Ceremony number: " + rs.getString("ceremony") + " - " +
+					"Category: " + rs.getString("category") + " - " +
+					"Name: " + rs.getString("name") + " - " +
+					"For film: " + rs.getString("film") + " - " +
+					"Winner: " + ((rs.getBoolean("winner")) ? "Yes" : "No"));
+		}
+		
+		return (!awardsAndInfo.isEmpty()) ? awardsAndInfo : null;
+	}
 }
